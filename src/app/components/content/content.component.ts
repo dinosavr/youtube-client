88e3ca17123fbb46efe0927944f3eabe9bd38b59
Alex Sidorov <promoabys@gmail.com>
2020-09-07 18:20:59 +0300

    fix: remove not used import

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-content',
--- src/app/modules/content-item-detail/content-item-detail.module.ts
+++ src/app/modules/content-item-detail/content-item-detail.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IResponseItems } from '../youtube-response-example/youtube-response-example.module';
 
 @NgModule({
   declarations: [],
