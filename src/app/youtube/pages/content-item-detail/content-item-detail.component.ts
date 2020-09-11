dbead76e7646a6715307d5f9ae2c4b6ede86eebf
Alex Sidorov <promoabys@gmail.com>
2020-09-11 03:42:34 +0300

    feat: detail video page

--- src/app/youtube/pages/content-item-detail/content-item-detail.component.ts
+++ src/app/youtube/pages/content-item-detail/content-item-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ContentService } from '../../../core/services/content.service';
+import { IResponseItems } from '../../models/youtube-response-example/youtube-response-example.module';
 
 @Component({
   selector: 'app-content-item-detail',
@@ -8,23 +10,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 // export class ContentItemDetailComponent implements OnInit {
 
-  export class ContentItemDetailComponent implements OnInit {
-    public userId: number;
+export class ContentItemDetailComponent implements OnInit {
+  public videoId: string;
+  public videoItem: IResponseItems;
+  public colorBottomBg: string;
 
-    constructor(public route: ActivatedRoute) {}
+  constructor(public route: ActivatedRoute, private contentService: ContentService) { }
 
-    public ngOnInit(): void {
-      this.route.params.subscribe(params => {
-        console.log('[User Route]', params);
-        this.userId = params.id;
-      });
+  public ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.videoId = params.id;
+    });
 
-      console.log('Snapshot:', this.route.snapshot.params);
-      // this.userId = this.route.snapshot.params.id;
-    }
-
-    public showInfo(): void {
-      console.log('Log Snapshot:', this.route.snapshot.params);
-    }
-
+    this.videoItem = this.getVideo(this.contentService.posts, this.videoId)[0];
   }
+
+  public getVideo(items: IResponseItems[], id: string): IResponseItems[] {
+
+    if (!items || !id) {
+      return items;
+    }
+
+    return items.filter(item => item.id.indexOf(id) !== -1);
+  }
+
+}
